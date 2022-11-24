b5d3f16c4dac60939fe001e2b9c452f0533fb06d
Felipe Castro <111576084+felipejmcastro@users.noreply.github.com>
2022-11-24 15:01:23 +0000

    Consulta ao banco de dados e inserção de dados

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-#Importa nosso arquivo Pessoa.py no diretorio model
+#Importa nosso arquivo Pessoa.py no diretório model
 from model.Pessoa import Pessoa
-
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
-felipe = Pessoa (1, "Felipe Castro")
+felipe = Pessoa(1, "Felipe Castro")
 print(felipe)
 
 #Quero mostrar só o nome
 print(felipe.nome)
 
+print("DAQUI PRA FRENTE É ACESSO AO BANCO...")
+
+#Chama o objeto de banco de dados
+db = Database()
+
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+
+#Criar um objeto com qualquer ator/atriz/diretor/diretora
+novo = Pessoa(0, "Denzel Washington")
+
+#Olha que simples...
+novo = pessoaDAO.save(novo)
+
+#consulta o banco de novo...
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+  
